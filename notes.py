b1401a8d0ff209b5b57e58a57b4b30df582fd5e6
Lizzie <74326237+cutiepie44@users.noreply.github.com>
2022-01-27 12:38:21 -0500

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,15 @@
+import os
+
+print('what do you want to do')
+print('1: take note')
+print('2: edit note')
+print('3: delete note')
+input0 = input('')
+
+if input0 == '1':
+    if True:
+        print("take your notes here")
+        notes = input("")
+        print('now name your note')
+        name = input('')
+
